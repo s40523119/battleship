347b467028473085a900968055220175c9494bce
s40523119 <40523119@gm.nfu.edu.tw>
2017-06-08 02:22:38 +0800

    play battleship

---
+++ battleship.js
@@ -0,0 +1,35 @@
+//DECLARE variables
+var location1 = 3;
+var location2 = 4;
+var location3 = 5;
+
+var guess;
+var hits = 0;
+var guesses = 0;
+
+var isSunk = false;
+
+//LOOP
+while (isSunk == false) {
+    guess = prompt("Ready Aim Shot! (input a number range of 0 to 6)");
+    if (guess < 0 || guess >6) {
+      alert("it's not avaliable");
+
+    } else {
+        guesses = guesses + 1;
+
+        if (guess == location1 || guess == location2 || guess == location3) {
+          hits = hits + 1;
+          alert("hit!!")
+          if (hits == 3) {
+            isSunk = true;
+            alert("you win the game!!")
+
+          }
+        }  else {
+          alert("miss!")
+        }
+    }
+}
+var stats = "you guessed " + guesses + " times, " + "Accuracy is " + (3/guesses);
+alert(stats);
